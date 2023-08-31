f8ccc41742b1ce64660a2075ce548c73d1cdcd56
tr33hz <treezmusic@yandex.ru>
2023-08-31 16:46:55 +0300

    Исправлена ошибка в пакете "exceptions"

---
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/ValidetionException.java
@@ -0,0 +1,7 @@
+package ru.yandex.practicum.filmorate.exceptions;
+
+public class ValidetionException extends RuntimeException{
+    public ValidetionException(String message) {
+        super(message);
+    }
+}
