359cde09ca463ff91e5114fcfd986fd437c40355
tr33hz <treezmusic@yandex.ru>
2023-08-31 16:49:14 +0300

    Удален пакет "exceptions"

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/ValidetionException.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.practicum.filmorate.exceptions;
-
-public class ValidetionException extends RuntimeException{
-    public ValidetionException(String message) {
-        super(message);
-    }
-}
