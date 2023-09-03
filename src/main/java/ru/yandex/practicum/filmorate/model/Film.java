07cbd8533e31e54452b4768b62c3d09a1809dda9
tr33hz <treezmusic@yandex.ru>
2023-09-03 19:13:38 +0300

    В пакете "controllers" ответ теперь формируется через ResponseEntity.В моделях добавлены аннотации @NoArgsConstructor и @AllArgsContructor

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -0,0 +1,31 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+import ru.yandex.practicum.filmorate.annotaions.InvalidFilmCreationAnnotaion;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.PositiveOrZero;
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+public class Film {
+
+    @PositiveOrZero
+    private int id;
+
+    @NotBlank(message = "Название фильма не может быть пустым")
+    private String name;
+
+    @Length(max = 200, message = "Описание фильма не должно быть более 200 символов")
+    private String description;
+
+    @InvalidFilmCreationAnnotaion(message = "Не соответствующая дата релиза")
+    private LocalDate releaseDate;
+
+    @Positive(message = "Продолжительность фильма должна быть положительной")
+    private int duration;
+}
