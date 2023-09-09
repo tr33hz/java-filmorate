57ace6b91f705f5f4a7d0a214011137d84ff5cb2
tr33hz <120664451+tr33hz@users.noreply.github.com>
2023-09-09 10:37:21 +0300

    Fixed problems of the checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -1,9 +1,11 @@
 package ru.yandex.practicum.filmorate.storage;
 
+import ru.yandex.practicum.filmorate.model.Film;
+
 import java.util.List;
 import java.util.Optional;
 
-public interface FilmStorage<Film> {
+public interface FilmStorage<T> {
 
     List<Film> getAll();
 
--- src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -1,9 +1,11 @@
 package ru.yandex.practicum.filmorate.storage;
 
+import ru.yandex.practicum.filmorate.model.User;
+
 import java.util.List;
 import java.util.Optional;
 
-public interface UserStorage<User> {
+public interface UserStorage<T> {
     List<User> getUsers();
 
     User createUser(User user);
